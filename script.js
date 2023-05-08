c849af622a53989e76138d8720d1157de1c69cc2
Michael <124568429+16bitYoda@users.noreply.github.com>
2023-05-08 14:52:14 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const btn = document.querySelector('.btn');
+//   const videoContainer = document.querySelector('.video-container');
+//   const close = document.querySelector('.close');
+
+//   btn.addEventListener('click',()=>{
+//     videoContainer.classList.add('show');
+//   })
+
+//   close.addEventListener('click',()=>{
+//     videoContainer.classList.remove('show');
+//   })
+// });
